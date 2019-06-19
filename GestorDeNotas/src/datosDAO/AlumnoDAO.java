683dd64206731324baccb2e540df6db2ba4c3ef5
“oldanirenzo” <renzo.oldani@alumnos.frm.utn.edu.ar>
2019-06-18 21:06:18 -0300

    Actualizo datosDAO

--- GestorDeNotas/src/datosDAO/AlumnoDAO.java
+++ GestorDeNotas/src/datosDAO/AlumnoDAO.java
@@ -1,14 +1,173 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package datosDAO;
 
-/**
- *
- * @author m3p
- */
-public class AlumnoDAO {
-    
+import datosDAO.SQLQuery;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import modelo.Alumno;
+
+public class AlumnoDAO extends SQLQuery {
+
+    public Alumno alum1;
+
+    public AlumnoDAO() {
+        try {
+            this.conectar("localhost", "proyectoMetodologia", "root", "root");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(AlumnoDAO.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Error de conexion.");
+        }
+    }
+
+    public Boolean agregarAlumnoDao(Alumno alu) throws SQLException {
+        String insertTableSQL = "INSERT INTO Alumno"
+                + "(dni_alu,nom_alu,ape_alu,domic_alu,tel_alu) VALUES"
+                + "(?,?,?,?,?)";
+        this.consulta = this.conexion.prepareStatement(insertTableSQL);
+        consulta.setLong(1, alu.getDni_alu());
+        consulta.setString(2, alu.getNom_alu());
+        consulta.setString(3, alu.getApe_alu());
+        consulta.setString(4, alu.getDomic_alu());
+        consulta.setString(5, alu.getTel_alu());
+
+        if (consulta.executeUpdate() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean eliminarAlumnos(Alumno alu) {
+        PreparedStatement elimAlumno = null;
+        PreparedStatement elimAlumnoNota = null;
+
+        String elimAluNot = "DELETE FROM Nota WHERE dni_alu_not = " + alu.getDni_alu();
+        String elimAlu = "DELETE FROM Alumno WHERE dni_alu = " + alu.getDni_alu();
+
+        try {
+            elimAlumnoNota = conexion.prepareStatement(elimAluNot);
+
+            elimAlumnoNota.executeUpdate();
+
+            elimAlumno = conexion.prepareStatement(elimAlu);
+
+            if (elimAlumno.executeUpdate() > 0) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (SQLException e1) {
+            return false;
+        }
+
+    }
+
+    public Alumno modificarAlumno(Alumno alumno) {
+        int modificado = 0;
+
+        if (alumno.getNom_alu().length() > 0) {
+
+            try {
+                this.conectar("localhost", "proyectoMetodologia", "root", "root");
+                this.consulta = this.conexion.prepareStatement("UPDATE Alumno SET nom_alu=? WHERE dni_alu=?");
+                consulta.setString(1, alumno.getNom_alu());
+                consulta.setLong(2, alumno.getDni_alu());
+                consulta.executeUpdate();
+                if (consulta.executeUpdate() > 0) {
+                    modificado++;
+                } else {
+                    modificado = -10;
+                }
+            } catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(Alumno.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        if (alumno.getApe_alu().length() > 0) {
+            try {
+                this.conectar("localhost", "proyectoMetodologia", "root", "root");
+                this.consulta = this.conexion.prepareStatement("UPDATE Alumno SET ape_alu=? WHERE dni_alu=?");
+                consulta.setString(1, alumno.getApe_alu());
+                consulta.setLong(2, alumno.getDni_alu());
+                consulta.execute();
+                if (consulta.executeUpdate() > 0) {
+                    modificado++;
+                } else {
+                    modificado = -10;
+                }
+            } catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(Alumno.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+        if (alumno.getDomic_alu().length() > 0) {
+            try {
+                this.conectar("localhost", "proyectoMetodologia", "root", "root");
+                this.consulta = this.conexion.prepareStatement("UPDATE Alumno SET domic_alu=? WHERE dni_alu=?");
+                consulta.setString(1, alumno.getDomic_alu());
+                consulta.setLong(2, alumno.getDni_alu());
+                consulta.execute();
+                if (consulta.executeUpdate() > 0) {
+                    modificado++;
+                } else {
+                    modificado = -10;
+                }
+            } catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(Alumno.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        if (alumno.getTel_alu().length() > 0) {
+            try {
+                this.conectar("localhost", "proyectoMetodologia", "root", "root");
+                this.consulta = this.conexion.prepareStatement("UPDATE Alumno SET tel_alu=? WHERE dni_alu=?");
+                consulta.setString(1, alumno.getTel_alu());
+                consulta.setLong(2, alumno.getDni_alu());
+                consulta.executeUpdate();
+                if (consulta.executeUpdate() > 0) {
+                    modificado++;
+                } else {
+                    modificado = -10;
+                }
+            } catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(Alumno.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        if (modificado > 0) {
+            JOptionPane.showMessageDialog(null, "Los datos han sido modificados con éxito", "Operacion Exitosa", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "No se ha podido realizar la modificacion de los datos.\n"
+                    + "Intentelo nuevamente.", "Error en la operación", JOptionPane.ERROR_MESSAGE);
+        }
+
+        return alumno;
+    }
+
+    public ArrayList<Alumno> getListadoAlumnos() {
+        ArrayList<Alumno> listadoAlumnos = new ArrayList<>();
+        String sql = "SELECT * FROM Alumno";
+
+        try {
+            this.consulta = this.conexion.prepareStatement(sql);
+            consulta.execute();
+
+            datos = consulta.executeQuery();
+
+            while (this.datos.next()) {
+                alum1 = new Alumno();
+                alum1.setDni_alu(this.datos.getLong("dni_alu"));
+                alum1.setNom_alu(this.datos.getString("nom_alu"));
+                alum1.setApe_alu(this.datos.getString("ape_alu"));
+                alum1.setDomic_alu(this.datos.getString("domic_alu"));
+                alum1.setTel_alu(this.datos.getString("tel_alu"));
+                listadoAlumnos.add(alum1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AlumnoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listadoAlumnos;
+    }
 }
--- GestorDeNotas/src/datosDAO/MateriaDAO.java
+++ GestorDeNotas/src/datosDAO/MateriaDAO.java
@@ -1,14 +1,200 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package datosDAO;
 
-/**
- *
- * @author m3p
- */
-public class MateriaDAO {
-    
+import datosDAO.SQLQuery;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import modelo.Materia;
+
+public class MateriaDAO extends SQLQuery {
+
+    public Materia mat;
+
+    public MateriaDAO() {
+        try {
+            this.conectar("localhost", "proyectoMetodologia", "root", "root");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(MateriaDAO.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Error de conexion.");
+        }
+    }
+
+    public Boolean agregarMateriaDAO(Materia mat) throws SQLException {
+        ArrayList<Long> dniProfesores = new ArrayList<>();
+        String sql = "SELECT dni_prof FROM Profesor";
+
+        try {
+            this.consulta = this.conexion.prepareStatement(sql);
+            consulta.execute();
+
+            datos = consulta.executeQuery();
+
+            while (this.datos.next()) {
+                long dni;
+                dni = (this.datos.getLong("dni_prof"));
+                dniProfesores.add(dni);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AlumnoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        int cont = 0;
+
+        String insertTableSQL = "SET FOREIGN_KEY_CHECKS =0";
+        consulta = this.conexion.prepareStatement(insertTableSQL);
+        consulta.executeUpdate();
+        insertTableSQL = "INSERT INTO Materia"
+                + "(cod_mat, descr_mat, cant_hor_mat, dni_prof_mat) VALUES"
+                + "(?,?,?,?)";
+        this.consulta = this.conexion.prepareStatement(insertTableSQL);
+        consulta.setInt(1, mat.getCod_mat());
+        consulta.setString(2, mat.getDescr_mat());
+        consulta.setInt(3, mat.getCant_hor_mat());
+        consulta.setLong(4, mat.getDni_prof_mat());
+        for (int i = 0; i < dniProfesores.size(); i++) {
+            if (dniProfesores.get(i) == mat.getDni_prof_mat()) {
+                cont++;
+            }
+        }
+        if (cont > 0) {
+            if (consulta.executeUpdate() > 0) {
+                insertTableSQL = "SET FOREIGN_KEY_CHECKS =1";
+                consulta = this.conexion.prepareStatement(insertTableSQL);
+                consulta.executeUpdate();
+                return true;
+
+            } else {
+
+                return false;
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "No se encontro un profesor que coincida con ese documento.");
+            JOptionPane.showMessageDialog(null, "Agrege ese profesor, y reintente.");
+            return false;
+        }
+    }
+
+    public boolean eliminarMateriaDAO(Materia mat) {
+
+        String elimMateria = "DELETE FROM Materia WHERE cod_mat = " + mat.getCod_mat();
+
+        try {
+            consulta = conexion.prepareStatement(elimMateria);
+
+            if (consulta.executeUpdate() > 0) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (SQLException e1) {
+            return false;
+        }
+
+    }
+
+    public Materia modificarMateriaDAO(Materia mat) {
+        int modificado = 0;
+
+        if (mat.getDescr_mat().length() > 0) {
+
+            try {
+                this.conectar("localhost", "proyectoMetodologia", "root", "root");
+                this.consulta = this.conexion.prepareStatement("UPDATE Materia SET descr_mat=? WHERE cod_mat=?");
+                consulta.setString(1, mat.getDescr_mat());
+                consulta.setLong(2, mat.getCod_mat());
+                consulta.executeUpdate();
+                if (consulta.executeUpdate() > 0) {
+                    modificado++;
+                } else {
+                    modificado = -10;
+                }
+            } catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(Materia.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        if (mat.getDescr_mat().length() > 0) {
+            try {
+                this.conectar("localhost", "proyectoMetodologia", "root", "root");
+                this.consulta = this.conexion.prepareStatement("UPDATE Materia SET descr_mat=? WHERE cod_mat=?");
+                consulta.setString(1, mat.getDescr_mat());
+                consulta.setLong(2, mat.getCod_mat());
+                consulta.execute();
+                if (consulta.executeUpdate() > 0) {
+                    modificado++;
+                } else {
+                    modificado = -10;
+                }
+            } catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(Materia.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+        if (mat.getCant_hor_mat() > 0) {
+            try {
+                this.conectar("localhost", "proyectoMetodologia", "root", "root");
+                this.consulta = this.conexion.prepareStatement("UPDATE Materia SET cant_hor_mat=? WHERE cod_mat=?");
+                consulta.setInt(1, mat.getCant_hor_mat());
+                consulta.setInt(2, mat.getCod_mat());
+                consulta.execute();
+                if (consulta.executeUpdate() > 0) {
+                    modificado++;
+                } else {
+                    modificado = -10;
+                }
+            } catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(Materia.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        if (mat.getDni_prof_mat() > 0) {
+            try {
+                this.conectar("localhost", "proyectoMetodologia", "root", "root");
+                this.consulta = this.conexion.prepareStatement("UPDATE Materia SET dni_prof_mat=? WHERE cod_mat=?");
+                consulta.setLong(1, mat.getDni_prof_mat());
+                consulta.setLong(2, mat.getCod_mat());
+                consulta.executeUpdate();
+                if (consulta.executeUpdate() > 0) {
+                    modificado++;
+                } else {
+                    modificado = -10;
+                }
+            } catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(Materia.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        if (modificado > 0) {
+            JOptionPane.showMessageDialog(null, "Los datos han sido modificados con éxito", "Operacion Exitosa", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "No se ha podido realizar la modificacion de los datos.\n"
+                    + "Intentelo nuevamente.", "Error en la operación", JOptionPane.ERROR_MESSAGE);
+        }
+
+        return mat;
+    }
+
+    public ArrayList<Materia> getListadoMateriasDAO() {
+        ArrayList<Materia> listadoMaterias = new ArrayList<>();
+        String sql = "SELECT * FROM Materia";
+
+        try {
+            this.consulta = this.conexion.prepareStatement(sql);
+            consulta.execute();
+
+            datos = consulta.executeQuery();
+
+            while (this.datos.next()) {
+                mat = new Materia();
+                mat.setCod_mat(this.datos.getInt("cod_mat"));
+                mat.setDescr_mat(this.datos.getString("descr_mat"));
+                mat.setCant_hor_mat(this.datos.getInt("cant_hor_mat"));
+                mat.setDni_prof_mat(this.datos.getLong("dni_prof_mat"));
+                listadoMaterias.add(mat);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MateriaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listadoMaterias;
+    }
 }
